99fad464f9fd9a7ff200971b0da9923f06a0c0b8
Neeraj Verma <neeraj.viman@gmail.com>
2019-08-08 22:21:12 +0530

    part of prev commit

--- main.go
+++ main.go
@@ -1,16 +1,24 @@
 package main
 
 import (
-	"fmt"
 	"io"
 	"io/ioutil"
 	"net/http"
+	"time"
 
-	"github.com/zenazn/goji"
+	"github.com/patrickmn/go-cache"
 	"github.com/imroc/req"
+	"github.com/zenazn/goji"
 )
 
+var myCache *cache.Cache
+
 func main() {
+
+	// Create a cache with a default expiration time of 5 minutes, and which
+	// purges expired items every 10 minutes
+	myCache = cache.New(3*time.Minute, 6*time.Minute)
+
 	// Add routes to the global handler
 	goji.Get("/whats-playing", whatsPlaying)
 
@@ -23,18 +31,26 @@ func main() {
 	// shutdown on SIGINT. Serve() is appropriate for both development and
 	// production.
 	goji.Serve()
-
 }
 
 // whatsPlaying route (GET "/"). Print a list of greets.
 func whatsPlaying(w http.ResponseWriter, r *http.Request) {
+
+	foo, found := myCache.Get("wow-music")
+	if found {
+		io.WriteString(w, foo.(string))
+		return
+	}
+
 	rawResp, _ := req.Get("https://api.listenbrainz.org//1/user/smurfpandey/playing-now")
 	resp := rawResp.Response()
-	fmt.Println(resp.StatusCode)
-	
+		
 	bodyBytes, _ := ioutil.ReadAll(resp.Body)
     
-    bodyString := string(bodyBytes)
+	bodyString := string(bodyBytes)
+	
+	// save in cache
+	myCache.Set("wow-music", bodyString, cache.DefaultExpiration)
 	
 	io.WriteString(w, bodyString)
 }
